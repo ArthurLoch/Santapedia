900f065924de9668b869e1df59dcfde59bb1401b
Arthur Deluchi Loch <arthur.d.loch@gmail.com>
2026-01-01 17:19:30 -0300

    Updating settings

--- Santapedia/santapedia/settings.py
+++ Santapedia/santapedia/settings.py
@@ -35,6 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 
 # Application definition
